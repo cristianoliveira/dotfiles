17a34be6a1be21b2768f79dfd40ccece17af73da
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-12-18 20:48:37 -0200

    feature: install tmux

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-ln -s ~/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/vim ~/.vim
@@ -12,5 +12,15 @@ source antigen.zsh
 git clone git@github.com:clvv/fasd.git /tmp/fasd
 cd /tmp/fasd && sudo make install
 
+case $OSTYPE in
+  linux-gnu)  echo "Tmux LINUX" && sudo apt-get install tmux ;;
+  darwin*)  echo "Tmux OSX" && brew install tmux ;;
+  *)        echo "Not supported: $OSTYPE" ;;
+esac
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall
+vim +PluginInstall +qall
+
+echo "dotfiles executed."
+echo "Installed: zsh, vim and tmux"
+echo "Configured."
