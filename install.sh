1b1c94af5ed5aa862e4923aa1ae436838a799182
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-12-18 07:03:05 -0200

    feature: install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl https://cdn.rawgit.com/zsh-users/antigen/v1.3.1/bin/antigen.zsh > antigen.zsh
+source antigen.zsh
+
+ln -s .tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim ~/.vim
