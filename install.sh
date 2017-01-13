74970d7e33d528b4889a34caf0fe1f02e5e0986b
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-01-12 23:49:10 -0200

    feature: remove app installing from script and add theme

--- install.sh
+++ install.sh
@@ -5,19 +5,7 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitigore ~/.gitigore
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-curl https://cdn.rawgit.com/zsh-users/antigen/v1.3.1/bin/antigen.zsh > antigen.zsh
-source antigen.zsh
-
-git clone git@github.com:clvv/fasd.git /tmp/fasd
-cd /tmp/fasd && sudo make install
-
-case $OSTYPE in
-  linux-gnu)  echo "Tmux LINUX" && sudo apt-get install tmux ;;
-  darwin*)  echo "Tmux OSX" && brew install tmux ;;
-  *)        echo "Not supported: $OSTYPE" ;;
-esac
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
