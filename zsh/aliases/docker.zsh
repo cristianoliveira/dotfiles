afb9262625790965f80ea5ce14437dccdbf0a8d4
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-02-04 09:24:46 +0100

    feature: new aliaeses for git and docker-compose

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -1,3 +1,8 @@
 alias docker-clean-containers='docker rm -f $(docker ps -a -q)'
 alias docker-clean-images='docker rmi -f $(docker images -a -q'
 alias docker-clean-volumes='docker volume rm $(docker volume ls -q)'
+
+# docker-compose
+alias dcc=docker-compose
+alias dccu=docker-compose up
+alias dccs=docker-compose stop
