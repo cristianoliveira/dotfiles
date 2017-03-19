be64b5cf6189c8a4f2d9f71906fd180505cd8351
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-03-19 15:11:49 -0300

    refactor: put gen-ctags for Rust at rust file

--- zsh/aliases/custom.zsh
+++ zsh/aliases/custom.zsh
@@ -16,7 +16,6 @@ alias zz='fasd_cd -d -i' # interactive directory jump
 
 alias :slp='pmset sleepnow'
 
-alias gen-ctags-rust='ctags -f tags --options=/Users/crosa/.dotfiles/ctags.rust'
 alias habits='\history -1000 -1 | sed "s/^[[:space:]]*[0-9]*[[:space:]]*//" | sort | uniq -c | sort -n -r | head -n 30'
 
 alias wruby='find **/*.rb | entr '
--- zsh/aliases/rust.zsh
+++ zsh/aliases/rust.zsh
@@ -1,3 +1,5 @@
 alias cb='cargo build'
 alias ct='cargo test'
 alias cr='cargo run'
+
+alias gen-ctags-rust="ctags -f tags --options=/Users/$USER/.dotfiles/ctags.rust"
