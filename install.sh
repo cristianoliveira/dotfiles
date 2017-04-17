e93ffa115de3011208124424874de494c2eb2c2e
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-04-17 14:28:38 +0200

    feature: add rbenv configs on install

--- install.sh
+++ install.sh
@@ -5,10 +5,16 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitigore ~/.gitigore
 
+echo "Installing zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+echo "Installing rbenv"
+git clone https://github.com/rbenv/rbenv.git rbenv
+ln -s ~/.dotfiles/rbenv ~/.rbenv
+eval "$(~/.rbenv/bin/rbenv init -)"
 
 echo "dotfiles executed."
 echo "Installed: zsh, vim and tmux"
