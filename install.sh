6d0ac124b0d5953135622b73193ad93c6f36beff
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-05-03 14:46:51 +0200

    feature: adding ctags to install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/git/gitignore ~/.gitignore
+ln -s ~/.dotfiles/ctags ~/.ctags
 
 echo "Installing zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &
