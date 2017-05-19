897e47d2e131fbf2d2aa85f50a4dfea7c0d6d2af
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-05-19 11:05:22 +0200

    feature: add docker aliases

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -1,8 +1,11 @@
-alias docker-clean-containers='docker rm -f $(docker ps -a -q)'
-alias docker-clean-images='docker rmi -f $(docker images -a -q'
-alias docker-clean-volumes='docker volume rm $(docker volume ls -q)'
+alias dk=docker
+
+alias dk-clean-containers='docker rm -f $(docker ps -a -q)'
+alias dk-clean-images='docker rmi -f $(docker images -a -q'
+alias dk-clean-volumes='docker volume rm $(docker volume ls -q)'
 
 # docker-compose
-alias dcc=docker-compose
-alias dccu=docker-compose up
-alias dccs=docker-compose stop
+alias dcc='docker-compose'
+alias dccu='docker-compose up'
+alias dccd='docker-compose up'
+alias dccs='docker-compose stop'
