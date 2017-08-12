27d5796d7fc031e6d22ab97a5e1149911bea8656
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-08-12 16:03:43 +0200

    feature: Add autoload configuration

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,7 +1,8 @@
 # Link an shared script to be used with ./autostart/MyDotfiles.desktop
-sudo ln -s ~/.dotfiles/linux/onlogin.sh /usr/local/share/onlogin.sh
+sudo ln -s $HOME/.dotfiles/linux/onlogin.sh /usr/local/share/onlogin.sh
+ln -s $HOME/.dotfiles/linux/autostart/MyDotfiles.desktop ~/.config/autostart/MyDotfiles.desktop
 
-sh ./install-packages.sh
+sh $HOME/.dotfiles/linux/install-packages.sh
 
 # Change key delay rate and repeat
 xset r rate 180 40
