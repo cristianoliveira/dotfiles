2d9877c4f6c2bc1101702f6b5754328f5c9cba16
Cristian Oliveira <cristian.oliveiradarosa@barzahlen.de>
2017-10-25 18:10:27 +0200

    feature: git helper function for update

--- zsh/func/github.zsh
+++ zsh/func/github.zsh
@@ -1,3 +1,5 @@
 # clone a repository simple doing:
 # githubclone 'cristianoliveira/dotfiles'
 function githubclone { `git clone git@github.com:$1.git` }
+
+function gitupdatefolder { `find . -maxdepth 1 -type d -print -execdir git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;` }
