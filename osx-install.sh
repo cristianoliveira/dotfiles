b4468f36fcc1a42eb7db7f9cd0252f90dfef894b
Cristian Oliveira <contato@cristianoliveira.com.br>
2019-08-02 12:01:19 +0200

    Add notification cli for OSX

--- osx-install.sh
+++ osx-install.sh
@@ -14,3 +14,6 @@ sh $HOME/.dotfiles/osx/configure-osx.sh squall
 
 echo "\n\n\n\n\n----------------------Setup-------------------\n\n\n\n\n"
 sh $HOME/.dotfiles/setup.sh
+
+echo "\n\n\n\n\n----------------------Installing CLIs-------------------\n\n\n\n\n"
+ln -s $HOME/.dotfiles/osx/bin/notifyme /usr/local/bin/notifyme
