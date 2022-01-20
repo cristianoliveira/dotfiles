2baaced285ab0369ceac8bd7082ae41f76283c95
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-01-20 19:05:17 +0100

    ssh add link

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,2 +1,7 @@
 ssh-keygen -t rsa -b 4096 -C "contato@cristianoliveira.com.br"
+
+echo "---------------------------"
 cat ~/.ssh/id_rsa.pub
+echo "---------------------------"
+
+echo "Copy and add the content above to https://github.com/settings/ssh/new"
