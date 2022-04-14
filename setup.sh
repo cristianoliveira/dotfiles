117869d09397f690f84736ee1fb0c5f44ec5e83d
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-04-14 09:16:01 +0200

    feat: introduce neovim

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 echo "Add all symbolic links..."
+NVIM_CONFIG_PATH=~/.config/nvim
+mkdir -p $NVIM_CONFIG_PATH
+
 mv ~/.tmux.conf  ~/old-tmux.conf
 mv ~/.vimrc ~/old-vimrc
 mv ~/.zshrc ~/old-zshrc
 mv ~/.vim ~/old-vim
 mv ~/.gitignore ~/old-gitignore
 mv ~/.ctags ~/old-ctags
+mv $NVIM_CONFIG_PATH/init.vim $NVIM_CONFIG_PATH/old-init.vim
 
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
@@ -13,12 +17,16 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitignore ~/.gitignore
 ln -s ~/.dotfiles/ctags ~/.ctags
+ln -s ~/.dotfiles/vim/init.vim $NVIM_CONFIG_PATH/init.vim
 
 echo "Istalling tmux-setup helper"
 ln -s ~/tmux/tmux-setup /usr/local/bin/tmux-setup
 
 echo "Installing zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &
+
+echo "Installing Language Server for TS and JS"
+npm install -g typescript-language-server typescript
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
