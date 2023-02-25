fd4b2f122f3babfbc021cf9fa3a4f0dc75cdbb82
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-02-25 19:30:43 +0100

    refact: move performance configs to lua

--- vim/lua/settings/performance.lua
+++ vim/lua/settings/performance.lua
@@ -1,2 +1,7 @@
 vim.o.ttyfast = true
 vim.o.lazyredraw = true
+
+-- Performance fix for Typescript
+-- https://jameschambers.co.uk/vim-typescript-slow
+-- Avoid using old regex implementation for code highlight which is slooow
+vim.o.re = 0
