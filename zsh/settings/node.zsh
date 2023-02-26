19f2e36b4768eefdc230b2ac7b105ddf1bc70b7e
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-02-26 21:32:19 +0100

    feat(zsh): improve startup time to ~300ms
    (cherry picked from commit fbfd628d54460d7e03dd28f28f7974783d0a993b)

--- zsh/settings/node.zsh
+++ zsh/settings/node.zsh
@@ -1,4 +1,13 @@
 echo "Setting node env..."
 
-export NVM_LAZY_LOAD=true
-source $HOME/.zsh-nvm/zsh-nvm.plugin.zsh
+function lazy_load_nvm() {
+  export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  export NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist
+}
+
+function nvm () {
+  lazy_load_nvm
+  nvm $@
+}
