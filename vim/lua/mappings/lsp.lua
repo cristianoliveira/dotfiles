e431e21885743a0c52120b3a9fbcfa4660edeb7d
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-02-28 15:43:45 +0100

    feat(vim): lsp go to definition center the screen

--- vim/lua/mappings/lsp.lua
+++ vim/lua/mappings/lsp.lua
@@ -4,6 +4,8 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
+nmap("<C-]>", "<C-]>zz")
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 Lsp_on_attach = function(_, bufnr)
