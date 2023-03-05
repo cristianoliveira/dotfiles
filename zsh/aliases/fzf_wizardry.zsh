474add45b8b8e1c7a95e1880daaf299eef86f801
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-03-05 11:34:45 +0100

    feat(git/fzf): use fzf to pick a branch to checkout

--- zsh/aliases/fzf_wizardry.zsh
+++ zsh/aliases/fzf_wizardry.zsh
@@ -11,3 +11,9 @@ local fzf_git_log_formatted="$git_log_formatted | fzf"
 alias fgl="$fzf_git_log_formatted | awk '{print \$1}'"
 alias fglt10="$fzf_git_log_formatted | head -n 10 | fzf | awk '{print \$1}'"
 alias fglt20="$fzf_git_log_formatted | head -n 20 | fzf | awk '{print \$1}'"
+
+# fzf git checkout (pick branch)
+local git_branch_formatted="git branch --sort=committerdate --format='%(refname:short) [%(committerdate:relative)]'"
+local alias_fgb="$git_branch_formatted | fzf | awk '{print \$1}'"
+alias fgb=$alias_fgb
+alias fgch="git checkout \$($alias_fgb)"
