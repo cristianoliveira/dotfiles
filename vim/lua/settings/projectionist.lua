3a29ef92e37e327f627bf69bab5d65b59304bb31
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-05-19 23:22:39 +0200

    refact(vim): move projectionist settings to lua

---
+++ vim/lua/settings/projectionist.lua
@@ -0,0 +1,36 @@
+vim.g.projectionist_heuristics = {
+  ['package.json'] = {
+    ['*.js'] = {
+      ['alternate'] = { '{}.spec.js' }
+    },
+    ['*.spec.js'] = {
+      ['alternate'] = { '{}.js' }
+    },
+    ['*.ts'] = {
+      ['alternate'] = { '{}.spec.ts' }
+    },
+    ['*.spec.ts'] = {
+      ['alternate'] = { '{}.ts' }
+    },
+    ['*.tsx'] = {
+      ['alternate'] = { '{}.spec.tsx' }
+    },
+    ['*.spec.tsx'] = {
+      ['alternate'] = { '{}.tsx' }
+    },
+    ['*.jsx'] = {
+      ['alternate'] = { '{}.spec.jsx' }
+    },
+    ['*.spec.jsx'] = {
+      ['alternate'] = { '{}.jsx' }
+    }
+  },
+  ['go.mod'] = {
+    ['*.go'] = {
+      ['alternate'] = { '{}_test.go' }
+    },
+    ['*_test.go'] = {
+      ['alternate'] = { '{}.go' }
+    }
+  }
+}
