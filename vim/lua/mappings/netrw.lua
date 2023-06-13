84bcca2c41777d8de1a87061848a446db4254305
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-06-13 21:25:48 +0200

    feat(vim): disable backup files

--- vim/lua/mappings/netrw.lua
+++ vim/lua/mappings/netrw.lua
@@ -4,10 +4,11 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = 'netrw',
   callback = function()
     local buffer = vim.api.nvim_get_current_buf()
-    vim.keymap.set('n', '<C-u>', '-^', { buffer = buffer, remap = true })
+    vim.keymap.set('n', 'u', '-^', { buffer = buffer, remap = true })
     vim.keymap.set('n', 'mm', 'R', { buffer = buffer, remap = true })
     vim.keymap.set('n', 'md', 'D', { buffer = buffer, remap = true })
     vim.keymap.set('n', '<C-r>', '<C-l>', { buffer = buffer, noremap = true })
+    vim.keymap.set('n', 'dd', 'D', { buffer = buffer, remap = true })
   end,
 })
 
--- vim/lua/settings/vim.lua
+++ vim/lua/settings/vim.lua
@@ -1,4 +1,12 @@
 vim.opt.timeoutlen = 1300
 
 -- Working on a monorepop is fun!
+-- See: https://github.com/vim/vim/issues/2049
 vim.opt.mmp=5000
+
+-- Set vim backup off
+vim.opt.backup = false
+vim.opt.backupcopy = 'no'
+vim.opt.backupdir="~/vimtmp//,."
+vim.opt.directory="~/vimtmp//,."
+vim.opt.writebackup = false
