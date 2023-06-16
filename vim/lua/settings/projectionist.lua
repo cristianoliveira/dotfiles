27cc11a9624aae7f22d7fb27ec9a3e70a5b59cba
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-06-16 12:59:00 +0200

    feat(vim): expands list of alternative files for js (projectionist)

--- vim/lua/settings/projectionist.lua
+++ vim/lua/settings/projectionist.lua
@@ -1,21 +1,30 @@
 vim.g.projectionist_heuristics = {
   ['package.json'] = {
     ['*.js'] = {
-      ['alternate'] = { '{}.spec.js' }
+      ['alternate'] = { '{}.spec.js', '{}.test.js' }
+    },
+    ['*.test.js'] = {
+      ['alternate'] = { '{}.js' }
     },
     ['*.spec.js'] = {
       ['alternate'] = { '{}.js' }
     },
     ['*.ts'] = {
-      ['alternate'] = { '{}.spec.ts' }
+      ['alternate'] = { '{}.spec.ts', '{}.test.ts' }
     },
     ['*.spec.ts'] = {
       ['alternate'] = { '{}.ts' }
     },
+    ['*.test.ts'] = {
+      ['alternate'] = { '{}.ts' }
+    },
     ['*.tsx'] = {
-      ['alternate'] = { '{}.spec.tsx' }
+      ['alternate'] = { '{}.spec.tsx', '{}.test.tsx' }
     },
     ['*.spec.tsx'] = {
+      ['alternate'] = { '{}.tsx' }
+    },
+    ['*.test.tsx'] = {
       ['alternate'] = { '{}.tsx' }
     },
     ['*.jsx'] = {
