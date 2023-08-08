2c053b4f9d62602da93c990eb440ad1040f32bb3
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-08 08:02:20 +0200

    feat(vim): change mappings for netrw
    (cherry picked from commit 7006d744b48005e7e8e017f07040a058e3f9e972)

--- vim/lua/mappings/netrw.lua
+++ vim/lua/mappings/netrw.lua
@@ -8,16 +8,19 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.keymap.set('n', 'mm', 'R', { buffer = buffer, remap = true })
     vim.keymap.set('n', 'md', 'D', { buffer = buffer, remap = true })
     vim.keymap.set('n', '<C-r>', '<C-l>', { buffer = buffer, noremap = true })
+    vim.keymap.set('n', '<C-l>', '<C-W><C-l>', { buffer = buffer, noremap = true })
     vim.keymap.set('n', 'dd', 'D', { buffer = buffer, remap = true })
+    vim.keymap.set('n', '<leader>dd', function()
+      vim.cmd('silent! call netrw#NetrwDelete()')
+    end, { buffer = buffer, remap = true })
   end,
 })
 
 -- Folder navigation
 -- Open Ex in the current file dir
-nmap('<C-e>', ':e <C-R>=expand("%:p:h") . "/" <CR>')
+nmap('<leader>ee', ':e <C-R>=expand("%:p:h") . "/" <CR><CR>')
 -- Open Ex in the project root folder
-nmap("<C-e>~", ":e . <CR>")
-nmap('<leader>e', ':e <C-R>=expand("%:p:h") . "/" <CR>')
+nmap('<leader>er', ':e . <CR>')
 
 -- Fd specific filders
 nmap('<leader>FA', ':Fd api')
