937a94ae0c238c5e076abc34e19650b6d6da29d1
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-12 19:11:08 +0200

    nvim(feat): grep app plugin selected text mapping

--- nvim/lua/customization/plugins/grepapp/init.lua
+++ nvim/lua/customization/plugins/grepapp/init.lua
@@ -1,4 +1,14 @@
 function Grepapp(args)
   io.popen("open https://grep.app/search?q=".. args)
 end
+
+function selected_text()
+  vim.cmd.normal('"gy')
+  return vim.fn.getreg('g')
+end
+
+vim.keymap.set('v', '<leader>ga', function()
+  Grepapp(selected_text())
+end, { noremap = true, silent = true, desc = 'Search [G]rep [A]pp for selected text' })
+
 vim.cmd("command! -nargs=1 Grepapp lua Grepapp(<f-args>)")
