e2184672a0d600e029dce021e8bdc9720968e524
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-23 13:17:53 +0200

    feat(zsh): introduces asdf as version management

---
+++ scripts-install/asdf.zsh
@@ -0,0 +1,3 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
---
+++ zsh/settings/asdf.zsh
@@ -0,0 +1,6 @@
+. "$HOME/.asdf/asdf.sh"
+
+# append completions to fpath
+fpath=(${ASDF_DIR}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit && compinit
