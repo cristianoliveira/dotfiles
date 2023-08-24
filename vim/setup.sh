1fb0ecee959a9af12d1f012eb981f7e2dd5550fb
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-24 18:29:53 +0200

    fix(setup): missing home path

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,13 +2,13 @@
 
 NVIM_CONFIG_PATH=~/.config/nvim
 mv $NVIM_CONFIG_PATH $DOTFILES_BKP_PATH
-ln -s "$HOME_PATH"/.dotfiles/vim $NVIM_CONFIG_PATH
+ln -s "$HOME"/.dotfiles/vim $NVIM_CONFIG_PATH
 
-mv "$HOME_PATH"/.vimrc $DOTFILES_BKP_PATH/old-vimrc
-mv "$HOME_PATH"/.vim $DOTFILES_BKP_PATH/old-vim
+mv "$HrfE_PATH"/.vimrc $DOTFILES_BKP_PATH/old-vimrc
+mv "$HOME"/.vim $DOTFILES_BKP_PATH/old-vim
 
-ln -s "$HOME_PATH"/.dotfiles/vim "$HOME_PATH"/.vim
-ln -s "$HOME_PATH"/.dotfiles/vim/vimrc "$HOME_PATH"/.vimrc
+ln -s "$HOME"/.dotfiles/vim "$HOME"/.vim
+ln -s "$HOME"/.dotfiles/vim/vimrc "$HOME"/.vimrc
 
-git clone https://github.com/gmarik/Vundle.vim.git "$HOME_PATH"/.dotfiles/vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git "$HOME"/.dotfiles/vim/bundle/Vundle.vim
 vim +PluginInstall +qall
