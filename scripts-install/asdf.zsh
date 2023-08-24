db6da5449972554c8c2f297a5cf6f3b9d439ba17
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-24 19:48:38 +0200

    feat(setup): uses asdf for version managment
    (cherry picked from commit 33e8df0ad5f37fa18abf7a29bde6e7e3b668b3a5)

--- scripts-install/asdf.zsh
+++ scripts-install/asdf.zsh
@@ -1,3 +1,6 @@
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add python
