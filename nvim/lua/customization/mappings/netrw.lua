caaf2a1ff02149ce81d5d5beb37aca210dac07e4
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-25 14:56:15 +0200

    feat(vim): adds mapping for f* to manipulate files

--- nvim/lua/customization/mappings/netrw.lua
+++ nvim/lua/customization/mappings/netrw.lua
@@ -5,10 +5,16 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     local buffer = vim.api.nvim_get_current_buf()
     vim.keymap.set('n', 'u', '-^', { buffer = buffer, remap = true })
-    vim.keymap.set('n', 'mm', 'R', { buffer = buffer, remap = true })
-    vim.keymap.set('n', 'md', 'D', { buffer = buffer, remap = true })
     vim.keymap.set('n', '<C-r>', '<C-l>', { buffer = buffer, noremap = true })
-    vim.keymap.set('n', '<leader>dd', 'D', { buffer = buffer, remap = true })
+
+    vim.keymap.set('n', 'fm', 'mf', { buffer = buffer, remap = true })
+    vim.keymap.set('n', 'fd', 'D', { buffer = buffer, remap = true })
+    vim.keymap.set('n', 'fc', 'mtmc', {
+      buffer = buffer,
+      remap = true,
+      desc = 'Copy marked files to current directory'
+    })
+    vim.keymap.set('n', 'fr', 'R', { buffer = buffer, remap = true })
   end,
 })
 
