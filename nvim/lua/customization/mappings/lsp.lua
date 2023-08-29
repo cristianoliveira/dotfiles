497d0b4367f7344467aa8cde76f9e7717ba06d2c
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-29 11:23:40 +0200

    fix(vim): removes duplicated format on save trigger

--- nvim/lua/customization/mappings/lsp.lua
+++ nvim/lua/customization/mappings/lsp.lua
@@ -56,8 +56,4 @@ Lsp_on_attach = function(_, bufnr)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<leader>ff', function() vim.lsp.buf.format { async = true } end, bufopts)
 
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
 end
