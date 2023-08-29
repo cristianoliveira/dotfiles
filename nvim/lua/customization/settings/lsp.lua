09f5063796e673e0860a98e243410cacc4c705bf
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-29 11:25:06 +0200

    delete-me: adds htmx-lsp config

--- nvim/lua/customization/settings/lsp.lua
+++ nvim/lua/customization/settings/lsp.lua
@@ -4,6 +4,40 @@ local lspconfig = require('lspconfig')
 local mason_lspconfig = require("mason-lspconfig")
 local capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+local util = require("lspconfig.util")
+local configs = require("lspconfig.configs")
+
+configs.htmx_lsp = {
+  default_config = {
+    -- Paste here the path to the lsp bin
+    cmd = {
+      "/Users/cristianoliveira/other/htmx-lsp/target/debug/htmx-lsp",
+      "--file",
+      "/Users/cristianoliveira/other/htmx-lsp/log.log",
+      "--level",
+      "DEBUG",
+    },
+    filetypes = { "html" },
+    root_dir = util.path.dirname,
+    autostart = true,
+  },
+
+  docs = {
+    description = [[
+    Language Server Protocol for Conventional Commits.
+    ]],
+    default_config = {
+      root_dir = [[root_pattern(".git")]],
+    },
+  },
+}
+
+lspconfig.htmx_lsp.setup {
+  on_attach = Lsp_on_attach, -- see ../mappings/lsp.lua
+  flags = lsp_flags,
+}
+
+
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
