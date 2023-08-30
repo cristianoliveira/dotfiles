fb5f7a4bac1d6970253cbe0eb5ed82e6aae1f2ed
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-08-30 23:19:08 +0200

    feat(vim): alias for nightly version

---
+++ nvim/install-nightly-macos.sh
@@ -0,0 +1,4 @@
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+xattr -c ./nvim-macos.tar.gz
+tar xzvf ./nvim-macos.tar.gz
+./nvim-macos/bin/nvim
--- zsh/aliases/nvim.zsh
+++ zsh/aliases/nvim.zsh
@@ -1,3 +1,7 @@
 alias vim="nvim"
 alias vi="nvim"
 alias vimdiff='nvim -d'
+
+# neovim nightly
+alias vimn="~/.dotfiles/nvim-macos/bin/nvim"
+alias nvimn="~/.dotfiles/nvim-macos/bin/nvim"
