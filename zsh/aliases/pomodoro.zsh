be5b3e772b133c4f96bfeeee09e06516f8cac68d
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-11-17 09:23:54 +0100

    feat: adds pomodoro cli

---
+++ zsh/aliases/pomodoro.zsh
@@ -0,0 +1,3 @@
+alias work="pomodoro 25 '$TASK' && notifyme '$TASK, time to pause'"
+alias pause="pomodoro 5 'Pause' && notifyme 'Pause finished, time to work'"
+alias longpause="pomodoro 15 'Long Pause' && notifyme 'Pause finished, time to work'"
