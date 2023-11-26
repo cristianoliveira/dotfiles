f3e3d8fda35aede89abc15f7a64654aa522a4a06
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-11-26 16:18:54 +0100

    feat(linux): add setup scripts for 2023

---
+++ linux/scripts/install-guis.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Browsers
+
+wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+
+sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: $distro
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+
+sudo apt update
+sudo apt install librewolf -y
+
+# Ulauncher (alfred)
+sudo add-apt-repository ppa:agornostal/ulauncher
+sudo apt update
+sudo apt install ulauncher
+
+# Bitwarden
+sudo snap install bitwarden
