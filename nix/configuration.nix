2a4f24469681909c2d39b7ef6d86ff59d3bbaaca
cristianoliveira <contato@cristianoliveira.com.br>
2024-03-17 14:03:06 +0100

    feat(nix): adds tmux

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -94,9 +94,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # Development environment
+    vim
     neovim 
     git
+    tmux
+
+    # Essential pkgs
     curl
     wget
 
