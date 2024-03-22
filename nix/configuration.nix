0e41b8830575d7f9a76215f7fc9082d0b7bb9bd7
cristianoliveira <contato@cristianoliveira.com.br>
2024-03-22 11:47:31 +0100

    feat(nix): enables touchpad support

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -78,7 +78,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.cristianoliveira = {
