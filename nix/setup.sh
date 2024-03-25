41c6960b3b66ae4a6398f3050ef8bb8c0fcc7b6e
cristianoliveira <contato@cristianoliveira.com.br>
2024-03-25 09:33:18 +0100

    feat(nix): update setup script

--- nix/setup.sh
+++ nix/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo ln -s $HOME/.dotfiles/nix/configuration.nix /etc/nixos/
+sudo ln -s $HOME/.dotfiles/nix/nixos/configuration.nix /etc/nixos/
+sudo ln -s $HOME/.dotfiles/nix/nixos/hardware-configuration.nix /etc/nixos/
+
+sudo nixos-rebuild switch --flake $HOME/.dotfiles/nix#nixos
