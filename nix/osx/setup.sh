5380f38d36b8469b0bd893e38012c72f1d25750d
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-26 20:30:02 +0100

    feat(osx): basic nix config

--- nix/osx/setup.sh
+++ nix/osx/setup.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-# Install Nix
-# sh <(curl -L https://nixos.org/nix/install)
-
-echo "Install Nix Darwin"
+read -p "Do you want to install Nix? (y/n) " ANSWER
+if [[ $ANSWER =~ ^[Yy]$ ]]; then
+  echo "Installing Nix"
+  sh <(curl -L https://nixos.org/nix/install)
+else
+  echo "Skipping Nix installation"
+fi
 
 read -p "Do you want to install Nix Darwin? (y/n) " ANSWER
 if [[ $ANSWER =~ ^[Yy]$ ]]; then
@@ -13,3 +16,10 @@ if [[ $ANSWER =~ ^[Yy]$ ]]; then
 else
   echo "Skipping Nix Darwin installation"
 fi
+
+echo "Linking Nix Darwin configuration"
+mkdir -p "$HOME/.nixpkgs"
+rm -f "$HOME/.nixpkgs/darwin-configuration.nix"
+ln -s "$HOME/.dotfiles/nix/osx/configuration.nix" "$HOME/.nixpkgs/darwin-configuration.nix"
+
+echo "--Done--"
