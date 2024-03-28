ad3952953851cfe492568ff68367b53c1fedc4d9
cristianoliveira <contato@cristianoliveira.com.br>
2024-03-28 21:57:39 +0100

    fix(nixos): correct syslink for sway/i3status/fonts

--- nix/nixos/i3status/setup.sh
+++ nix/nixos/i3status/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Linking config to $HOME/.config/i3status"
-mv $HOME/.config/i3status/config $HOME/.config/i3status/config.bak
-ln -s $HOME/.dotfiles/nix/nixos/i4status $HOME/.config/
+mv -f $HOME/.config/i3status/config $HOME/.config/i3status/config.bak
+ln -s $HOME/.dotfiles/nix/nixos/i3status $HOME/.config/
