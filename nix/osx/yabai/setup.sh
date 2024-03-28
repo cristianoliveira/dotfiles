cb7fc31a78f9fe788ab993002b17ce09eaeaed0a
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-28 19:04:48 +0100

    fix(nix/osx): yabai setup script syslink in $HOME

--- nix/osx/yabai/setup.sh
+++ nix/osx/yabai/setup.sh
@@ -3,14 +3,9 @@
 echo "Setting up yabai - tiling window manager"
 
 # Link yabai configuration
-mv $HOME/.yabairc $HOME/.yabairc.bak
-mv $HOME/.skhdrc $HOME/.skhdrc.bak
-ln -sf $HOME/.dotfiles/nix/osx/yabai/yabairc $HOME/.yabairc
-
-ln -sf $HOME/.dotfiles/nix/osx/yabai/skhdrc $HOME/.skhdrc
-
-# Installing via nix doesn't allow changing the config file path
-sudo mv /etc/skhdrc /etc/skhdrc.bak
-sudo ln -sf $HOME/.dotfiles/nix/osx/yabai/skhdrc /etc/skhdrc
+mv "$HOME/.yabairc" "$HOME/.yabairc.bak"
+mv "$HOME/.skhdrc" "$HOME/.skhdrc.bak"
+ln -sf "$HOME/.dotfiles/nix/osx/yabai/yabairc" "$HOME/.yabairc"
+ln -sf "$HOME/.dotfiles/nix/osx/yabai/skhdrc" "$HOME/.skhdrc"
 
 echo "yabai setup complete"
