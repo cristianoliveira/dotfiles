70d241a57681a78fb43cf2b52a0236ec5736e81c
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-28 14:00:28 +0100

    feat(vim): fixes find replace command cursor

--- nvim/lua/customization/mappings/find_replace.lua
+++ nvim/lua/customization/mappings/find_replace.lua
@@ -1,6 +1,11 @@
 nmap("<leader>rff", ":Ack \"\"<Left>", "[R]efactor [f]ind [f]iles")
-nmap("<leader>rfe", ":cfdo %s/<C-r>///g<left><left> | update", "Refactor [f]ind [e]dit")
-nmap("<leader>rfc", ":cfdo %s/<C-r>///gc<left><left><left> | update", "Refactor [f]ind [c]hange")
+
+local left = "<left>"
+for _ = 0, 11 do
+  left = left .. "<left>"
+end
+nmap("<leader>rfe", ":cfdo %s/<C-r>///g | update ".. left, "Refactor [f]ind [e]dit")
+nmap("<leader>rfc", ":cfdo %s/<C-r>///gc | update " .. left, "Refactor [f]ind [c]hange")
 
 nmap("<leader>rr", ":%s/<C-r>///g<left><left>")
 vmap("<leader>rr", ":s/<C-r>///g<left><left>")
