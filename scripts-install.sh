fcdfca48f1d01383b8e828e12730ac581a92fee8
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-28 21:04:35 +0100

    chore: clean up dead code

--- scripts-install.sh
+++
@@ -1,9 +0,0 @@
-set -e # fail on errorgf
-set -u # do not allow unset variables
-
-ls $HOME/.dotfiles/setup/
-for script in $HOME/.dotfiles/setup/*; do
-  echo "running $script"
-  bash "$script"
-done
-echo "All scripts were executed."
