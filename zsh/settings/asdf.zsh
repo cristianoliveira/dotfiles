a76a1b31cf13ffc3b37633f13f88f34ee143a12c
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-28 20:44:31 +0100

    chore: cleanup asdf configs

--- zsh/settings/asdf.zsh
+++
@@ -1,6 +0,0 @@
-if [ -z "$NIX_ENV" ]; then
-   . "$HOME/.asdf/asdf.sh"
-
-   # append completions to fpath
-   fpath=(${ASDF_DIR}/completions $fpath)
-fi
--- zsh/settings/golang.zsh
+++ zsh/settings/golang.zsh
@@ -1,6 +1,4 @@
 if [ -z "$NIX_ENV" ]; then
    export GOPATH="$HOME/golang"
    export PATH=$GOPATH/bin:$PATH
-
-   . ~/.asdf/plugins/golang/set-env.zsh
 fi
