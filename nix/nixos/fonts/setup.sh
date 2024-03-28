94b5195d0dc25d2daa8a260efd6af0819a323407
cristianoliveira <contato@cristianoliveira.com.br>
2024-03-28 21:57:39 +0100

    fix(nixos): correct syslink for sway/i3status/fonts

--- nix/nixos/fonts/setup.sh
+++ nix/nixos/fonts/setup.sh
@@ -2,6 +2,8 @@
 #
 # TODO run this script upon nix setup
 
+echo "Setting up Monaco font"
 mkdir -p $HOME/.local/share/fonts
-cp -s $HOME/.dotfiles/nix/fonts/* $HOME/.local/share/fonts
+mv -f $HOME/.local/share/fonts $HOME/.local/share/fonts.bak
+ln -s $HOME/.dotfiles/nix/nixos/fonts $HOME/.local/share/fonts
 fc-cache -f -v
--- nix/nixos/sway/setup.sh
+++ nix/nixos/sway/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Linking config to $HOME/.config/sway"
-mv $HOME/.config/sway $HOME/.config/sway.bak
+mv -f $HOME/.config/sway $HOME/.config/sway.bak
 ln -s $HOME/.dotfiles/nix/nixos/sway $HOME/.config/
