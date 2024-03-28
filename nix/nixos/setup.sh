8ffe48a4df934d9f7717b39370b1f6cd1dbfc33c
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-28 13:26:14 +0100

    refact(nixos): move all related config files to nixos
    (cherry picked from commit 072c22e1971c9163c76db573c99b74457b124e41)

--- nix/nixos/setup.sh
+++ nix/nixos/setup.sh
@@ -7,9 +7,9 @@ sudo ln -s $HOME/.dotfiles/nix/nixos/hardware-configuration.nix /etc/nixos/
 
 # link all configs in ~/.config
 mkdir -p $HOME/.config
-$HOME/.dotfiles/nix/sway/setup.sh
-$HOME/.dotfiles/nix/i3status/setup.sh
-$HOME/.dotfiles/nix/fonts/setup.sh
+$HOME/.dotfiles/nix/nixos/sway/setup.sh
+$HOME/.dotfiles/nix/nixos/i3status/setup.sh
+$HOME/.dotfiles/nix/nixos/fonts/setup.sh
 
 if ! command -v nix-env &> /dev/null; then
   echo "Nix is not installed. Make sure to install it."
