f8858534addffc0a09c2fe59e7ddb24a33a402d2
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-03-30 10:37:18 +0100

    feat(nix/osx): align alacritty version between linux/osx
    (cherry picked from commit d2d08a35182c72870f711808b7da8e48664e773d)

---
+++ nix/osx/alacritty/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Since alacritty is installed as a binary, we need to create a symlink to the
+# ~/Applications directory so that it can be found by alfred.
+
+# check if alacritty is installed
+if ! command -v alacritty &> /dev/null; then
+  echo "Alacritty is not installed. Please run 'nix/rebuild.sh'"
+  exit 1
+fi
+
+mkdir ~/Applications/Alacritty.app
+ln -s "$(which alacritty)" ~/Applications/Alacritty.app
