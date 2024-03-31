a520d8f48ecd1954a1f7aee198df97c1ec8295f4
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-01 00:29:35 +0200

    feat(nixos): adds cristianoliveira/nixpkgs for installing custom pkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,14 +2,26 @@
   description = "Nixos config flake - v1";
 
   inputs = {
+    # Linux
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    co-pkgs = {
+      url = "github:cristianoliveira/nixpkgs";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, co-pkgs, ... }:
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        ({ config, pkgs, ... }: { 
+          # Injects mypkgs into nixpkgs as custom
+          nixpkgs.overlays = [ 
+            (final: prev: { mypkgs = import co-pkgs { inherit pkgs; }; })
+          ];
+        })
         ./nixos/configuration.nix
       ];
     };
