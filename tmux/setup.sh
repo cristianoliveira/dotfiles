dc1d1884711f5425a4a81f683bab361fa1c6cb4b
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-21 20:34:25 +0200

    fix(tmux): check if config exists

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -2,5 +2,7 @@
 
 echo "Setting up tmux configurations."
 mkdir -p "$HOME"/.config
-mv -f "$HOME"/.tmux.conf "$HOME/.tmux.conf.bkp-$(date +%s)"
+#!/bin/sh
+
+[ -d "$HOME"/.config/.tmux.conf ] && mv -f "$HOME"/.tmux.conf "$HOME/.tmux.conf.bkp-$(date +%s)"
 ln -s "$HOME"/.dotfiles/tmux/tmux.conf "$HOME"/.tmux.conf
