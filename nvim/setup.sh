437ce67f50f72580ce42249e705fa6d4108fb721
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-21 20:28:43 +0200

    feat(nixos): missing wallpapper & nvim pipenv

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -4,4 +4,5 @@ NVIM_CONFIG_PATH=~/.config/nvim
 mv $NVIM_CONFIG_PATH $DOTFILES_BKP_PATH
 ln -s "$HOME"/.dotfiles/nvim $NVIM_CONFIG_PATH
 
-python3 -m pip install --user --upgrade pynvim
+# before nix
+# pip install --user --upgrade pynvim
