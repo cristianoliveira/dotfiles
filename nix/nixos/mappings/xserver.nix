a56efd289e571f54db704654d903daf372ee8baa
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-22 19:40:36 +0200

    feat(nixos): allow configuring keyboard everywhere

--- nix/nixos/mappings/xserver.nix
+++ nix/nixos/mappings/xserver.nix
@@ -13,10 +13,13 @@
     xkbOptions = "altwin:swap_lalt_lwin";
 
     # Keyboard repeat rate
-    autoRepeatDelay = 200;
+    autoRepeatDelay = 100;
     autoRepeatInterval = 10;
 
     # Enable touchpad support (enabled default in most desktopManager).
     libinput.enable = true;
   };
+
+  # Allow applying the same settings to outside of X11
+  console.useXkbConfig = true;
 }
