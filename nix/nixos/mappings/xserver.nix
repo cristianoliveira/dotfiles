f600afecb41544b2f6fd921eeb1c795cf745e347
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-22 21:23:39 +0200

    feat(nixos): start using sddm instead gdm

--- nix/nixos/mappings/xserver.nix
+++ nix/nixos/mappings/xserver.nix
@@ -4,20 +4,22 @@
   services.xserver = {
     # Enable the X11 windowing system.
     enable = true;
-    # Enable the GNOME Desktop Environment.
-    displayManager.gdm.enable = true;
-
     # Configure keymap in X11
     layout = "us";
     xkbVariant = "altgr-intl";
     xkbOptions = "altwin:swap_lalt_lwin";
 
     # Keyboard repeat rate
-    autoRepeatDelay = 100;
-    autoRepeatInterval = 10;
+    # autoRepeatDelay = 100;
+    # autoRepeatInterval = 10;
 
     # Enable touchpad support (enabled default in most desktopManager).
     libinput.enable = true;
+
+    # Enable the Desktop manager for logins
+    displayManager.sddm.enable = true;
+
+    exportConfiguration = true;
   };
 
   # Allow applying the same settings to outside of X11
