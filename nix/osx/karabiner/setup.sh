b727e4a99ba0337bb1be63569114a3fa71cc693a
cristianoliveira <contato@cristianoliveira.com.br>
2024-06-28 21:12:09 +0200

    fix(osx): setup karabiner file path
    (cherry picked from commit 51d7562760d1bc5e20bc532ee9616f6c9b70919e)

--- nix/osx/karabiner/setup.sh
+++ nix/osx/karabiner/setup.sh
@@ -4,4 +4,4 @@ echo "Setting up Karabiner configurations."
 
 mkdir -p "$HOME"/.config
 mv -f "$HOME"/.config/karabiner "$HOME"/.config/karabiner.bkp
-ln -s "$HOME"/.dotfiles/resources/karabiner "$HOME"/.config/karabiner
+ln -s "$HOME"/.dotfiles/nix/osx/karabiner "$HOME"/.config/karabiner
