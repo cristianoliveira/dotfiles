b5a083d3f147dec8c066a636aaf08b5d8aac52ae
cristianoliveira <contato@cristianoliveira.com.br>
2024-07-03 11:12:23 +0200

    feat(alias): adds direnv use flake alias

--- zsh/aliases/nix.zsh
+++ zsh/aliases/nix.zsh
@@ -2,4 +2,8 @@ alias nss="nix-shell --run 'zsh' shell.nix"
 alias nsh="nix-shell --run 'zsh' shell.nix"
 
 alias denv="echo 'use nix' > .envrc && direnv allow"
+# Use flake
+alias denvf="echo 'use flake' > .envrc && direnv allow"
+# Use nix-shell
+alias denvs="echo 'use nix' > .envrc && direnv allow"
 alias denvrm="rm .envrc"
