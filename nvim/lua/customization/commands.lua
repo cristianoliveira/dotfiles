3e92d1e68bfc2a1c804963b5ea17fb0435178c36
cristianoliveira <contato@cristianoliveira.com.br>
2024-07-22 13:51:06 +0200

    feat(vim): adds command :Do to run _scripts/do.sh

--- nvim/lua/customization/commands.lua
+++ nvim/lua/customization/commands.lua
@@ -5,3 +5,11 @@ require("customization/commands/obsidian")
 vim.cmd("command! -nargs=0 CFormat lua vim.lsp.buf.format()")
 
 vim.cmd("command! -nargs=0 NvimEdit :e $MYVIMRC")
+
+
+-- Usually when I am working on a project I have a script that I run over and over again
+-- I take this and create a simple command to run it
+-- with :Do
+--
+-- The folder `_scripts` is in the root of the project and ignore by git
+vim.cmd("command! -nargs=0 Do !bash _scripts/do.sh")
