f4b7a042d42b600677059be6c20b1bfc253fac5d
cristianoliveira <contato@cristianoliveira.com.br>
2024-08-05 07:16:55 +0200

    chore(workflow): adds ignore direnv

--- nix/shared/security.nix
+++ nix/shared/security.nix
@@ -1,7 +1,13 @@
 # Security related packages
+# FIXME this module isn't working as expected
+# there is an issue with pinentry
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    gnupg
-    pinentry # Required to generate gpg keys
-  ];
+
+  # To generate GPG keys
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    # pinentryFlavor = "gtk2";
+    pinentryPackage = pkgs.pinentry;
+  };
 }
