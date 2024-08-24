ec4d7fb4a0aac331dcd2f3f1b37728e4f5944a3c
cristianoliveira <contato@cristianoliveira.com.br>
2024-08-24 15:34:13 +0200

    fix(nix): update bootloader config

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -20,11 +20,8 @@
     ];
 
   # Bootloader.
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-    useOSProber = true;
-  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
