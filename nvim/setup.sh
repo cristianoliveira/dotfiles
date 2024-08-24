420b01ffd76c3f39bfe942d24e8e39e9d6e3b4cf
cristianoliveira <contato@cristianoliveira.com.br>
2024-08-24 15:46:52 +0200

    fix(nix): issue with ctags setup

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -4,11 +4,6 @@ set -e
 
 echo "Configuring nvim"
 
-if [ -d "$HOME"/.config/nvim ]; then
-  echo "Backing up your current configs: nvim"
-  mv "$HOME"/.config/nvim /tmp/"$BACKUPNAME"
-fi
-
 if [ -d "$HOME"/.local/share/nvim ]; then
   echo "Backing up your current state: mazon & lazy.nvim"
   mv "$HOME"/.local/share/nvim /tmp/"$BACKUPNAME"/sharenvim
@@ -18,11 +13,19 @@ mkdir -p "$HOME"/.local/share/nvim
 git clone --filter=blob:none --branch=stable \
   https://github.com/folke/lazy.nvim.git "$HOME"/.local/share/nvim/lazy
 
+echo "Linking config to $HOME/.config/nvim"
+if [ -f "$HOME"/.config/nvim ]; then
+  echo "Backing up your current configs: nvim"
+  mv "$HOME"/.config/nvim /tmp/"$BACKUPNAME"
+fi
 ln -s "$HOME"/.dotfiles/nvim "$HOME"/.config/nvim
 
 # before nix
 # pip install --user --upgrade pynvim
 
-
 echo "Setting up ctags"
+if [ -f "$HOME"/.ctags ]; then
+  echo "Backing up your current configs: ctags"
+  mv "$HOME"/.ctags /tmp/"$BACKUPNAME"
+fi
 ln -s "$HOME"/.dotfiles/ctags "$HOME"/.ctags
