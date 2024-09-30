b0195b1ac95c89a1443e78f14ce642aac997eacc
cristianoliveira <contato@cristianoliveira.com.br>
2024-09-30 13:21:45 +0200

    fix(nix/osx): makes nix paths with higher priority than osx's

--- nix/osx/configuration.nix
+++ nix/osx/configuration.nix
@@ -89,6 +89,13 @@
         plugins=(git vi-mode history-substring-search)
         source $ZSH/oh-my-zsh.sh
       '';
+
+      loginShellInit = ''
+        # See https://github.com/LnL7/nix-darwin/issues/122
+        # Workaround for macos because it sets the system path with higher priority
+        # than nix paths this will reverse the order of the path's sections
+        PATH=$(echo $PATH | sed 's/:/\n/g' | tac | tr "\n" ":")
+      '';
     };
   };
 
