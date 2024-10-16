2eb5ed6902bf7a993c393132bbaffa47706b2e6e
cristianoliveira <contato@cristianoliveira.com.br>
2024-10-16 21:18:45 +0200

    feat(alias): adds github gh aliases

---
+++ zsh/aliases/github.zsh
@@ -0,0 +1,9 @@
+# pull requests
+alias ghp='gh pr'
+alias ghpc='gh pr checks'
+alias ghpw='gh pr view'
+alias ghpww='gh pr view -w'
+
+# workflow
+alias ghwl='gh workflow list'
+alias ghw='gh workflow'
