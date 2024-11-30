2d429deedac447097ade71ac6d2c1d6b102aad34
cristianoliveira <contato@cristianoliveira.com.br>
2024-12-01 00:09:44 +0100

    feat(nixos): use layout mac for keyboard

--- nix/nixos/mappings/keyd.nix
+++ nix/nixos/mappings/keyd.nix
@@ -5,7 +5,6 @@
   # Key bindings for the following changes:
   # - CapsLock is Control when held, Escape when tapped
   # - RightAlt + hjkl as arrow keys
-  # - Option + aeiou for accents
 
   services = {
     keyd = {
@@ -18,14 +17,15 @@
 
             ## rightalt_hjkl layer
             # RightAlt + hjkl as arrow keys, fallback to Alt
-            "rightalt_hjkl:G" = {
+            "rightalt_hjkl:A" = {
               h = "left";
               j = "down";
               k = "up";
               l = "right";
             };
 
-            ## option_accents layer
+            #### NOTE: This is not being used but will leave as example 
+            # option_accents layer
             # Dead keys like in MacOS with option key 
             # Option + aeiou or falback to Super/Meta
             "option_accents:S" = {
@@ -34,31 +34,17 @@
               a = "macro(G-`)"; # grave
               i = "macro(G-6)"; # circumflex
               u = "macro(G-\")"; # diaeresis
+              c = "macro(G-,)"; # cedilla
             };
 
             main = {
               capslock = "overload(control, esc)";
               rightalt = "layer(rightalt_hjkl)";
-              leftmeta = "layer(option_accents)";
+              # leftmeta = "layer(option_accents)";
             };
           };
         };
       };
     };
-
-
-    # This didn't work, so check ../sway/config
-    # xserver = {
-    #   autoRepeatDelay = 1200;
-    #   autoRepeatInterval = 20;
-    #   xkb = {
-    #     layout = "us";
-    #     model = "pc104";
-    #     variant = "altgr-intl";
-    #     # FIXME this doesn't work. Bug on nix options, or overriden by xorg.conf?
-    #     options = "altwin:swap_lalt_lwin";
-    #   };
-    # };
-
   };
 }
