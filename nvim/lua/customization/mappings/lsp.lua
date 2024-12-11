923f020c3ac40f51f515293e5f6abd49d9750841
cristianoliveira <contato@cristianoliveira.com.br>
2024-12-11 18:37:08 +0100

    refact(vim): use custom function with description

--- nvim/lua/customization/mappings/lsp.lua
+++ nvim/lua/customization/mappings/lsp.lua
@@ -19,7 +19,7 @@ Lsp_on_attach = function(_, bufnr)
       desc = 'LSP: ' .. desc
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    vim.keymap.set('n', keys, func, { noremap = true, silent = true, buffer = bufnr, desc = desc })
   end
 
   -- Enable completion triggered by <c-x><c-o>
@@ -27,33 +27,22 @@ Lsp_on_attach = function(_, bufnr)
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   lspnmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   lspnmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
   lspnmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   lspnmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  lspnmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  lspnmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   lspnmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   lspnmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
   lspnmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  lspnmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd')
+  lspnmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove')
+  lspnmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, "[W]orkspace [L]ist")
 
   -- See `:help K` for why this keymap
   lspnmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   lspnmap('<C-K>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<leader>sgh', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>ff', function() vim.lsp.buf.format { async = true } end, bufopts)
-
 end
