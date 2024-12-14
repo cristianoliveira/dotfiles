da8fa2ded072fe57fc68b32b39f3aba5e286f110
cristianoliveira <contato@cristianoliveira.com.br>
2024-12-14 13:20:37 +0100

    feat(vim): retire the remapping of hjlk for navigating windows

--- nvim/lua/customization/mappings/panels.lua
+++ nvim/lua/customization/mappings/panels.lua
@@ -3,12 +3,6 @@ nmap("<Leader>s",":split<CR><C-K>")
 nmap("<Leader>w",":split<CR>")
 nmap("<Leader>v",":vsplit<CR>")
 
--- Panel navigation with Vim j/k/l/h
-nnoremap("<C-J>","<C-W><C-J>")
-nnoremap("<C-K>","<C-W><C-K>")
-nnoremap("<C-L>","<C-W><C-L>")
-nnoremap("<C-H>","<C-W><C-H>")
-
 -- Close quickfix panel
 nnoremap("<C-w>w", ":ccl<CR>")
 
