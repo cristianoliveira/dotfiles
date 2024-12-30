b11f60813f585c9e7cf057f8eae349f0b9303dfa
cristianoliveira <contato@cristianoliveira.com.br>
2024-12-30 21:08:06 +0100

    fix(nix/wm): make sure to swaylock and run in background

--- nix/nixos/sway/bin/swayidle.sh
+++ nix/nixos/sway/bin/swayidle.sh
@@ -8,7 +8,7 @@ set -e # This is necessary to catch errors in commands
 # seconds, it will turn off your displays. When you resume, swayidle will unlock
 #
 swayidle -w \
-  timeout 180 'swaylock' \
+  timeout 180 'swaylock -f' \
   timeout 60 'pgrep swaylock && swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' \
-  before-sleep 'swaylock'
+  before-sleep 'swaylock -f'
