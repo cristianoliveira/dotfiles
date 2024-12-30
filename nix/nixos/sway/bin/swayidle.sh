19c2ed2a0b9aeb311adbf66773c75e101d6faf27
cristianoliveira <contato@cristianoliveira.com.br>
2024-12-30 12:30:57 +0100

    feat(nix/wm): on lid off suspend and lock screen

---
+++ nix/nixos/sway/bin/swayidle.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e # This is necessary to catch errors in commands
+
+# SWAYIDLE
+#
+# After 120 seconds of inactivity, swayidle will lock your screen. After 20 more
+# seconds, it will turn off your displays. When you resume, swayidle will unlock
+#
+swayidle -w \
+  timeout 180 'swaylock' \
+  timeout 60 'pgrep swaylock && swaymsg "output * dpms off"' \
+  resume 'swaymsg "output * dpms on"' \
+  before-sleep 'swaylock'
--- nix/nixos/sway/validate.sh
+++ nix/nixos/sway/validate.sh
@@ -3,6 +3,8 @@
 set -e # This is necessary to catch errors in commands
 set -o pipefail # This is necessary to catch errors in commands piped together
 
+export SWAYSOCK=$(ls /run/user/1000/sway-ipc.* | head -n 1)
+
 sway --validate
 
 swaymsg reload
