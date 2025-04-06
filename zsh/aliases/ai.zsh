93bff567a6631f0c173a804342ec1b9d5a0460a3
cristianoliveira <contato@cristianoliveira.com.br>
2025-04-06 16:38:49 +0200

    feat(aliases): adds new AI chat aliases for snippets and git changes
    - Introduced `aig-snippet` alias for AI chat functionality with the `dev-snippet` role.
    - Added `aig-with-git` alias to automatically include modified and untracked files in AI chat using git.

--- zsh/aliases/ai.zsh
+++ zsh/aliases/ai.zsh
@@ -1,2 +1,7 @@
 alias ai="aichat"
-alias aicsh="aichat -c -r %shell%"
+alias aish="aichat -c -r %shell%"
+
+# Generate ai outputs
+alias aig-test="aichat -c -r dev-tdd -f $(git ls-files --modified --exclude-standard --others) "
+alias aig-snippet="aichat -c -r dev-snippet"
+alias aig-with-git="aichat -f $(git ls-files --modified --exclude-standard --others) "
