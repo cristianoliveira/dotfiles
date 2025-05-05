7f86aefc4df8b934a84dda828326529a93ff88bf
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-05 16:36:01 +0200

    feat(stow): adds aichat Darwin custom install

---
+++ stow/aichat/after_install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "After install aichat"
+
+# If Darwin, create a link to config file in $HOME/Application Support/aichat/config.yaml
+if [ "$(uname)" = "Darwin" ]; then
+    if [ -d "$HOME/Library/Application Support/aichat" ]; then
+        # Skip if the directory already exists
+        echo "Skipping aichat config file link, directory already exists"
+    else 
+        ln -sf "$HOME/.config/aichat" "$HOME/Library/Application Support/"
+    fi
+else
+    echo "Skipping aichat config file link for non-Darwin OS"
+fi
