9bfab8829f11ecaf3491079c7856e6c2fcff4c19
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-07 06:50:13 +0200

    feat(stow): use stow on install
    (cherry picked from commit 9e7fa81de4309da53ab7ec22ed6291708f37c81a)

--- nix/install.sh
+++ nix/install.sh
@@ -19,20 +19,4 @@ fi
 ## First run setup
 bash "$HOME/.dotfiles/nix/$OSTARGET/install.sh"
 
-for setup in $(find $HOME/.dotfiles/nix/$OSTARGET -name "setup.sh"); do
-    echo "NixOs: Running $setup"
-    bash $setup
-done
-
-for setup in $(find $HOME/.dotfiles/nix/shared -name "setup.sh"); do
-    echo "Shared: running $setup"
-    bash $setup
-done
-
-# check for folders in the root level but only one level deep
-for setup in $(find $HOME/.dotfiles/ -maxdepth 2 -name "setup.sh"); do
-    if [ "$setup" != "$HOME/.dotfiles/setup.sh" ]; then
-        echo "Running $setup"
-        bash $setup
-    fi
-done
+bash "$HOME/.dotfiles/stow/install.sh"
