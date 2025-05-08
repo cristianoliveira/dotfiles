6ab4477a8b9e7650650487f9e3d403e47ceffc11
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-08 08:06:24 +0200

    feat(zsh): adds npm aliases

--- zsh/aliases/javascript.zsh
+++ zsh/aliases/javascript.zsh
@@ -6,3 +6,10 @@ alias :Y=yarn
 alias yarnw='yarn workspace'
 alias yarnwp='yarn workspace $PACKAGE'
 alias :yw='yarn workspace'
+
+alias :nr='npm run'
+alias npdev='npm run dev'
+alias npstart='npm run start'
+alias npbuild='npm run build'
+alias nptest='npm run test'
+alias nplint='npm run lint'
