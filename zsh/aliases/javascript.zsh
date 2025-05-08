6b6cf71aeaf0eb29dde530591b6c211f955af0b1
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-08 17:16:58 +0200

    feat(zsh): adds aliases npm, direnv
    (cherry picked from commit d60090c9174f938202ffadbe9ef42ea4a0ba4c3e)

--- zsh/aliases/javascript.zsh
+++ zsh/aliases/javascript.zsh
@@ -7,7 +7,7 @@ alias yarnw='yarn workspace'
 alias yarnwp='yarn workspace $PACKAGE'
 alias :yw='yarn workspace'
 
-alias :nr='npm run'
+alias nr='npm run'
 alias npdev='npm run dev'
 alias npstart='npm run start'
 alias npbuild='npm run build'
