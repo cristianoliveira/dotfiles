f839f9d7441403c3fe71368db576df3e78b44645
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-08 17:16:58 +0200

    feat(zsh): adds aliases npm, direnv
    (cherry picked from commit d60090c9174f938202ffadbe9ef42ea4a0ba4c3e)

--- zsh/aliases/nix.zsh
+++ zsh/aliases/nix.zsh
@@ -3,9 +3,12 @@ alias nsh="nix-shell --run 'zsh' shell.nix"
 
 alias denv="echo 'use nix' > .envrc && direnv allow"
 # Use flake
-alias denvf="echo 'use flake' > .envrc && direnv allow"
-# Use nix-shell
-alias denvs="echo 'use nix' > .envrc && direnv allow"
+alias dusef="echo 'use flake' > .envrc && direnv allow"
+# Use nix
+alias dusen="echo 'use nix' > .envrc && direnv allow"
+# Load .env if it exists
+alias duseenv="echo 'dotenv_if_exists' >> .envrc && direnv allow"
+
 alias denvrm="rm .envrc"
 
 alias dallow="direnv allow"
