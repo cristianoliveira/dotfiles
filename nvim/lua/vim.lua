43eca6d315e7816f93942f58f658bfb852c4cf65
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-08 14:08:56 +0200

    feat(nvim): set options with neovim api

--- nvim/lua/vim.lua
+++ nvim/lua/vim.lua
@@ -10,27 +10,24 @@ vim.opt.backupcopy = 'no'
 vim.opt.backupdir = "~/vimtmp//,."
 vim.opt.directory = "~/vimtmp//,."
 vim.opt.writebackup = false
+vim.opt.swapfile = false
 
 -- Set highlight on search
-vim.o.hlsearch = true
+vim.opt.hlsearch = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.opt.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
--- vim.o.clipboard = 'unnamedplus'
+-- vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
+vim.opt.breakindent = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
 
 -- Make left column at smalest size
 vim.wo.signcolumn = 'yes:1'
@@ -38,46 +35,61 @@ vim.wo.foldcolumn = '0'
 vim.wo.numberwidth = 1
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.opt.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 
-vim.o.colorcolumn = "80"
+vim.opt.colorcolumn = "80"
 
-vim.cmd [[set t_Co=256]]
-vim.cmd [[set encoding=utf-8]]
-vim.cmd [[set fileencodings=utf-8]]
+-- Terminal colors and encoding
+vim.opt.termguicolors = true
+vim.opt.encoding = "utf-8"
+vim.opt.fileencodings = { "utf-8" }
+vim.opt.background = "dark"
 
-vim.cmd [[set autoindent]]
-vim.cmd [[set smartindent]]
-vim.cmd [[set cindent]]
-vim.cmd [[set background=dark]]
-vim.cmd [[set expandtab]]
-vim.cmd [[set smarttab]]
-vim.cmd [[set shiftwidth=2]]
-vim.cmd [[set softtabstop=2]]
-vim.cmd [[set tabstop=2]]
-vim.cmd [[set wildignore=*/.git/*,*/tmp/*,*.swp,*.pyc,*/node_modules/*]]
-vim.cmd [[set ignorecase]]
-vim.cmd [[set smartcase]]
-vim.cmd [[set incsearch]]
-vim.cmd [[set shiftround]]
-vim.cmd [[set history=1000]]
-vim.cmd [[set undolevels=1000]]
-vim.cmd [[set noswapfile]]
-vim.cmd [[set nobackup]]
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.cindent = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftround = true
 
-vim.cmd [[set synmaxcol=512]]
+vim.opt.wildignore = { 
+  "*/.git/*",
+  "*/tmp/*",
+  "*.swp", 
+  "*.pyc",
+  "*/node_modules/*"
+}
+
+-- Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+-- History and undo
+vim.opt.undofile = true
+vim.opt.history = 1000
+vim.opt.undolevels = 1000
+
+-- Syntax
+vim.opt.synmaxcol = 512
+
 vim.cmd [[syntax on]]
 vim.cmd [[syntax sync minlines=256]]
 
-vim.o.cursorline = true
-vim.o.linespace = 3
+vim.opt.cursorline = true
+vim.opt.linespace = 3
+
+-- Tab is 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 
 --> Navigation settings
 vim.opt.relativenumber = true
@@ -87,13 +99,13 @@ vim.g.netrw_liststyle = 0
 vim.wo.number = true
 
 --> Performance settings
-vim.o.ttyfast = true
-vim.o.lazyredraw = true
+vim.opt.ttyfast = true
+vim.opt.lazyredraw = true
 
 -- Performance fix for Typescript
 -- https://jameschambers.co.uk/vim-typescript-slow
 -- Avoid using old regex implementation for code highlight which is slooow
-vim.o.re = 0
+vim.opt.re = 0
 
 -- Working on a monorepop is fun!
 -- See: https://github.com/vim/vim/issues/2049
