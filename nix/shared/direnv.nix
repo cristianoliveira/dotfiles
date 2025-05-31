8000d7fdeae038acd2c9c568d9ffc29678e9b7d9
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-31 07:49:59 +0200

    fix(direnv): after update verbose is always on

--- nix/shared/direnv.nix
+++ nix/shared/direnv.nix
@@ -2,7 +2,9 @@
   programs.direnv = {
     enable = true;
     package = pkgs.direnv;
-    silent = true;
+    # FIXME: this is not working used the env var above
+    # See: https://github.com/direnv/direnv/issues/68#issuecomment-2812015043
+    silent = true; 
     nix-direnv = {
       enable = true;
       package = pkgs.nix-direnv;
