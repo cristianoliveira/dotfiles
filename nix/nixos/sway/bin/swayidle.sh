77a33ee03c98fc50b1fc4044262a24c879a2357e
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-02 18:32:42 +0200

    feat(sway): adds way to dismiss all notifications
    Summary:
    This commit adds a condition to prevent powering off the laptop display if it is already off, amends a key binding configuration, and updates a command in the Ulauncher shortcuts.
    
    Detailed Changes:
     - nix/nixos/sway/bin/swayidle.sh:
       - Introduced a condition to check if the laptop (eDP-1) is off before executing the power off command using swayidle.
     - nix/nixos/sway/config:
       - Corrected key binding for dismissing notifications, changing from `$mod+Ctrl+Shift+1` to `$lmod+1`.
     - nix/nixos/ulauncher/shortcuts.json:
       - Modified the command execution for the "Terminal>" shortcut to correctly run bash scripts in background.

--- nix/nixos/sway/bin/swayidle.sh
+++ nix/nixos/sway/bin/swayidle.sh
@@ -4,8 +4,10 @@ set -e
 
 pkill -x swayidle || true
 
+laptop_is_off='swaymsg -t get_outputs | jq -e \".[] | select(.active == false) | select(.name == \"eDP-1\")\"'
+
 swayidle -w \
   timeout 480 'swaylock -f' \
-  timeout 260 'swaymsg "output * power off"' \
+  timeout 260 "$laptop_is_off || swaymsg 'output * power off'"\
   resume 'swaymsg "output * power on"' \
   before-sleep 'swaylock -f'
