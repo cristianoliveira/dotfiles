20f497269c2e7725577b449b10f144159088c7a9
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-25 08:23:36 +0200

    feat(vim): ensure tree-sitter nix grammar

--- nvim/lua/customization/settings/treesitter.lua
+++ nvim/lua/customization/settings/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'nix' },
 
   modules = {},
   ignore_install = {},
