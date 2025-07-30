604bf12dc78f3de742ad2114e015e03744226e07
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-30 12:52:55 +0200

    feat(osxwm): Obsidian default menu shortcuts
    Summary:
    This commit introduces additional custom keyboard shortcuts for specific applications on macOS, enhancing user productivity and workflow.
    
    Detailed Changes:
     - nix/osx/applications-shortcuts.nix:
       - Introduced `editCommand` for default shortcut commands, though currently non-operational as expected.
       - Added `md.obsidian` section with specific shortcuts for the Obsidian app, including new note creation and navigation commands.
       - Updated `com.google.Chrome` and `com.brave.Browser` to use combined mappings with `editCommand`.

--- nix/osx/applications-shortcuts.nix
+++ nix/osx/applications-shortcuts.nix
@@ -1,5 +1,13 @@
 {
   system.defaults.CustomUserPreferences = let
+    editCommand = {
+      # Doesn 't work as expected, but it is the only way to set these
+      # "Cut" = "^x";
+      # "Copy" = "^c";
+      # "Paste" = "^v";
+      # "Paste and Match Style" = "^\$v";
+    };
+
     chromiumMappings = {
         # How to find these? Check the menu title in the app
 
@@ -19,23 +27,33 @@
 
         # Focus
         "Open Location..." = "^l";
-
-        # Page Actions 
-        "Cut" = "^x";
-        "Copy" = "^c";
-        "Paste" = "^v";
-        "Paste and Match Style" = "^\$v";
-        "Select All" = "^a";
-        "Redo" = "^y";
-        "Undo" = "^z";
-        "Find..." = "^f";
       };
   in {
     "com.google.Chrome" = {
-      "NSUserKeyEquivalents" = chromiumMappings;
+      "NSUserKeyEquivalents" = chromiumMappings // editCommand;
     };
     "com.brave.Browser" = {
-      "NSUserKeyEquivalents" = chromiumMappings;
+      "NSUserKeyEquivalents" = chromiumMappings // editCommand;
+    };
+
+    "md.obsidian" = {
+      "NSUserKeyEquivalents" = {
+        # Obsidian specific shortcuts
+        "New Note" = "^n";
+
+        # ctrl+p file search
+        "Open Quickly..." = "^p";
+        # "Search Files" = "^f";
+        # "Toggle Sidebars" = "^b";
+        # "Toggle Preview Mode" = "^m";
+        # "Toggle Edit Mode" = "^e";
+        # "Open Settings" = "^,";
+        # "Open Graph View" = "^g";
+
+        # Vim like navigation
+        "Navigate Back" = "^o";
+        "Navigate Forward" = "^i";
+      } // editCommand;
     };
   };
 }
