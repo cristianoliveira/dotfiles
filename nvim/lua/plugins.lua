1a539927764e785e6468bd4c82015309fc502772
__git_username__ <__git_email__>
2025-10-01 20:13:43 +0200

    chore(vim): remove unused plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -199,26 +199,26 @@ require('lazy').setup({
   -- Code runners and watchers
 
   -- Fzz watcher to run commands in parallel
-  (function()
-    local funzzy_vim_plugin = {
-      'cristianoliveira/funzzy.nvim',
-      build = 'cargo install funzzy'
-    };
-
-    -- Check if there is a local development version of funzzy
-    -- in the `~/other/funzzy.nvim` directory then change the pl
-    if vim.loop.fs_stat(vim.fn.expand('~/other/funzzy.nvim')) then
-      funzzy_vim_plugin = {
-        dir = "~/other/funzzy.nvim",
-        config = function()
-          vim.g.funzzy_bin = '~/.cargo/bin/fzz'
-          vim.g.fzz_bin = '~/.cargo/bin/fzz'
-        end,
-      }
-    end
-
-    return funzzy_vim_plugin
-  end)(),
+  -- (function()
+  --   local funzzy_vim_plugin = {
+  --     'cristianoliveira/funzzy.nvim',
+  --     build = 'cargo install funzzy'
+  --   };
+  --
+  --   -- Check if there is a local development version of funzzy
+  --   -- in the `~/other/funzzy.nvim` directory then change the pl
+  --   if vim.loop.fs_stat(vim.fn.expand('~/other/funzzy.nvim')) then
+  --     funzzy_vim_plugin = {
+  --       dir = "~/other/funzzy.nvim",
+  --       config = function()
+  --         vim.g.funzzy_bin = '~/.cargo/bin/fzz'
+  --         vim.g.fzz_bin = '~/.cargo/bin/fzz'
+  --       end,
+  --     }
+  --   end
+  --
+  --   return funzzy_vim_plugin
+  -- end)(),
 
   -- Curl.nvim to run curl commands within neovim
   -- Select curl and execute with :CurlOpen
