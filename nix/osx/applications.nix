b6d5f5fa29a7bba7a2baa748be5deeb3a0ce0c6e
cristianoliveira <public+github@cristianoliveira.com>
2025-10-03 07:52:44 +0200

    chore(osx): reformat applications.nix
    Summary:
    This commit refactors the format of the `nix/osx/applications.nix` file for improved readability and maintenance.
    
    Detailed Changes:
     - nix/osx/applications.nix:
       - Reformatted the structure for clarity by reorganizing the placement and indentation of the homebrew section.
       - Removed an unused import statement for `webapps` to clean up the code.

--- nix/osx/applications.nix
+++ nix/osx/applications.nix
@@ -1,52 +1,48 @@
-{ pkgs, ... }:
-  let 
-    webapps = import ../shared/webapps.nix { inherit pkgs; };
-  in {
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
-    environment.systemPackages = with pkgs; [
-      alacritty 
+{ pkgs, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    alacritty 
 
-      # FIXME this package is failing
-      # bitwarden-cli
+    # FIXME this package is failing
+    # bitwarden-cli
 
-      obsidian
+    obsidian
 
-      # webapps.chatgpt doesn't work in macOS
-      # webapps.youtube
+    # webapps.chatgpt doesn't work in macOS
+    # webapps.youtube
+  ];
+
+  # GUI applications via homebrew
+  homebrew = {
+    enable = true;
+
+    taps = [];
+
+    casks = [
+      "alfred" # Launcher
+
+      # Browsers
+      # "firefox"
+      "google-chrome"
+      # "google-chrome-canary"
+      "brave-browser"
+      "finicky" # Ensure to open links in a browser/profile based on rules
+
+      # Entertainment Apps
+      "spotify"
+      "slack"
+      "whatsapp"
+      "telegram"
+
+      "karabiner-elements"
+
+      # Others
+      "bitwarden"
+      # "veracrypt"
+      # "mullvadvpn"
+      # "tunnelblick" # VPN
+      "google-drive"
     ];
-
-    # GUI applications via homebrew
-    homebrew = {
-      enable = true;
-
-      taps = [];
-
-      casks = [
-        "alfred" # Launcher
-
-        # Browsers
-        "firefox"
-        "google-chrome"
-        # "google-chrome-canary"
-        "brave-browser"
-        "finicky" # Ensure to open links in a browser/profile based on rules
-
-        # Entertainment Apps
-        "spotify"
-        "slack"
-        "whatsapp"
-        "telegram"
-
-        "karabiner-elements"
-
-        # Others
-        "bitwarden"
-        # "veracrypt"
-        # "mullvadvpn"
-        # "tunnelblick" # VPN
-        "google-drive"
-      ];
-    };
-
-  }
+  };
+}
