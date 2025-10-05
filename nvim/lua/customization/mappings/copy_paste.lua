e07539e71b97eb498779ab62558ec1fb066e054d
cristianoliveira <public+github@cristianoliveira.com>
2025-10-05 13:59:52 +0200

    feat(nvim): add system clipboard mapping for Ctrl+C in visual mode
    Summary:
    Added a new key mapping to copy text to the system clipboard using Ctrl+C in visual mode to enhance usability and mimic common keyboard shortcuts.
    
    Detailed Changes:
     - nvim/lua/customization/mappings/copy_paste.lua:
       - Added a new mapping `vmap('<C-c>', '"+y')` for copying text to the system clipboard in visual mode.
    
    feat(nvim): add Ctrl-C mapping for visual mode
    
    Summary:
    This commit adds the Ctrl-C mapping in visual mode.
    
    Detailed Changes:
    - nvim/lua/customization/mappings/copy_paste.lua:
      - Added a comment explaining that Ctrl-C copies to the system clipboard in visual mode.

--- nvim/lua/customization/mappings/copy_paste.lua
+++ nvim/lua/customization/mappings/copy_paste.lua
@@ -7,7 +7,7 @@ vmap("<leader>d", "\"_c")
 
 -- system clipboard
 vmap('<Leader>y', '"+y')
-vmap('<C-c>', '"+y')
+vmap('<C-c>', '"+y') -- Ctrl-C to copy to system clipboard in visual mode
 -- FIXME In quarentine delete if not missed.
 -- vmap <Leader>d "+d
 -- nmap <Leader>p "+p
