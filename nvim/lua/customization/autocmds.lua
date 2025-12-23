20b4c72157e5acf60dc5ab42b318ca22c41fa93c
cristianoliveira <contato@cristianoliveira.com.br>
2025-12-23 12:14:19 +0100

    feat(nvim): remove random todo.txt autocmd
    This commit removes the autocmd that randomly opened todo.txt on VimEnter.
    
    Detailed Changes:
     - Implementation:
       - Removed the 'OpenTodo' autocmd group and its associated callback from autocmds.lua.

--- nvim/lua/customization/autocmds.lua
+++ nvim/lua/customization/autocmds.lua
@@ -16,23 +16,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   group = trim_group,
 })
 
---- On a random chance open the todo.txt if it is available
-local todo_group = vim.api.nvim_create_augroup('OpenTodo', { clear = true })
-math.randomseed(os.time())  -- Seed with current time
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    if vim.fn.filereadable('todo.txt') == 0 then
-      return
-    end
-    local chance = math.random() <= 0.5
-    if chance then
-      return
-    end
-    vim.cmd([[vsplit todo.txt]])
-  end,
-  group = todo_group,
-})
-
 -- format on save using vim.lsp.buf.format()
 -- local format_group = vim.api.nvim_create_augroup('FormatOnSave', { clear = true })
 -- vim.api.nvim_create_autocmd('BufWritePre', {
