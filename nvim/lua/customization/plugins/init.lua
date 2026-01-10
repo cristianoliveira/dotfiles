bb4ff0876814bf73534055c5a1dfc1609d77269d
cristianoliveira <contato@cristianoliveira.com.br>
2026-01-10 07:33:26 +0100

    chore(nvim): remove unused plugins
    This commit removes the aichat plugin and its associated files, as well as the fd plugin. The aichat plugin included several user commands for interacting with AI agents, which have been deleted.
    
    Detailed Changes:
     - Implementation:
       - Deleted aichat plugin files: airesearch.lua, common.lua, and init.lua.
       - Deleted fd plugin file: fd.lua.
       - Removed the require statement for fd.lua from plugins/init.lua.
     - Documentation:
       - Added usage comments to trouble.lua plugin configuration.

--- nvim/lua/customization/plugins/init.lua
+++ nvim/lua/customization/plugins/init.lua
@@ -1,4 +1,3 @@
 require("customization/plugins/grepapp")
-require("customization/plugins/fd")
 require("customization/plugins/reminder")
 require("customization/plugins/argpoon")
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,3 +1,7 @@
+-- Use `:Trouble diagnostics` to open the list of diagnostics
+-- Use `:Trouble lsp` to open the list of diagnostics from the current buffer
+-- Use `:Trouble quickfix` to open the list of quickfix items
+-- For more `:help Trouble`
 return {
   "folke/trouble.nvim",
   -- @type trouble.Config opts
