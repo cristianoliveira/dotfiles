8da3ca6f9cd621ce5afb171158f2efc1c1d1b6a3
cristianoliveira <public+github@cristianoliveira.com>
2026-01-13 22:04:33 +0100

    refactor(ai): new ai folder structure
    This commit refactors the AI folder structure to better. It moves the AI related files to a new `ai` folder, and updates the symlinks accordingly.
    Updated stow symlinks to reflect the new AI folder structure.

--- stow/ai/after_install.sh
+++ stow/ai/after_install.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-echo "After install aichat"
+echo "After install ai"
 
 # If Darwin, create a link to config file in $HOME/Application Support/aichat/config.yaml
 if [ "$(uname)" = "Darwin" ]; then
     if [ -d "$HOME/Library/Application Support/aichat" ]; then
         # Skip if the directory already exists
         echo "Skipping aichat config file link, directory already exists"
-    else 
+    else
         ln -sf "$HOME/.config/aichat" "$HOME/Library/Application Support/"
     fi
 else
