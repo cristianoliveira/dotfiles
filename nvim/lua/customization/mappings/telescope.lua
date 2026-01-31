c828ce3af427bd8be7f696863916a8c0b2b04415
cristianoliveira <public+github@cristianoliveira.com>
2026-01-31 10:56:11 +0100

    feat(nvim): ignore minified and map files in telescope
    This commit adds file ignore patterns to telescope to exclude minified and source map files from search results.
    
    Detailed Changes:
     - Implementation:
       - Added file_ignore_patterns to telescope defaults to ignore '%.min.*' and '%.map' files.

--- nvim/lua/customization/mappings/telescope.lua
+++ nvim/lua/customization/mappings/telescope.lua
@@ -45,6 +45,10 @@ end
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    file_ignore_patterns = {
+      '%.min.*',
+      '%.map',
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
