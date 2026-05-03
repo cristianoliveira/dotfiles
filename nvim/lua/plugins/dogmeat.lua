25ab7a6a1ce83f7c8f8321ab0f9e7e1ccecd60fa
cristianoliveira <contato@cristianoliveira.com.br>
2026-05-03 09:06:32 +0200

    chore(nvim): update plugin locks and dogmeat directory path
    This commit updates the plugin versions in the lazy-lock.json file and fixes the directory path for the dogmeat plugin.
    Detailed Changes:
     - Implementation:
       - Changed the dogmeat plugin directory path from `~/other/dogmeat.nvim` to `~/other/nvim.dogmeat`
     - Dependencies:
       - Updated gitsigns.nvim, mason.nvim, nvim-lspconfig, and vim-airline plugin versions in lazy-lock.json

--- nvim/lua/plugins/dogmeat.lua
+++ nvim/lua/plugins/dogmeat.lua
@@ -1,8 +1,8 @@
 -- If the plugin is in the file system attempt to load it
 -- Otherwise, use lazy loading
-if vim.fn.isdirectory(vim.fn.expand('~/other/dogmeat.nvim')) then
+if vim.fn.isdirectory(vim.fn.expand('~/other/nvim.dogmeat.')) then
   return {
-    dir = vim.fn.expand('~/other/dogmeat.nvim'),
+    dir = vim.fn.expand('~/other/nvim.dogmeat'),
     config = function()
       require('dogmeat.nvim_commands').setup()
     end,
