c2579d408cc4f73f7ce667b7c60a19b26c020f28
cristianoliveira <contato@cristianoliveira.com.br>
2026-07-14 15:01:57 +0200

    chore(git): add output directory to gitignore and update alacritty channel
    This commit adds an entry for the output/ directory to the global gitignore file
    and changes the alacritty package reference to the unstable channel in the NixOS
    configuration.
    
    - Changed alacritty package reference from stable to unstable channel in
      nix/osx/applications.nix.
    - Added `output/` directory to git/gitignore.

--- nix/osx/applications.nix
+++ nix/osx/applications.nix
@@ -2,7 +2,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    alacritty
+    unstable.alacritty
 
     # FIXME this package is failing
     # bitwarden-cli
